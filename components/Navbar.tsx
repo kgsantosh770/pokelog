48507457e251116e938cd36dd2bd79ab396308f6
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-04-13 14:34:36 +0530

    active nav link implementation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,37 @@
 import { Close, GitHub, Menu } from '@mui/icons-material';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react'
 
 const Navbar = () => {
 
-    const navigation = [
-        { name: 'Pokemon', href: '#', current: true },
-        { name: 'About', href: '#', current: false },
-        { name: 'Developed By', href: '#', current: false },
-    ]
+    const APP_NAME = process?.env?.NEXT_PUBLIC_APPNAME ?? 'Pokemons'
+    
+    const navigationRoutes: string[] = [APP_NAME, 'about']
+    const router = useRouter();
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+    function NavigationLink({ href, text }: { href: string, text: string }) {
+        const path = (href === `/${APP_NAME}`) ? '/' : href
+        const isActive = router.asPath === path
+        return (
+            <Link
+                href={href === "/home" ? "/" : path}
+                type='button'
+                className={`
+                    ${isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"} 
+                    rounded-md px-3 py-2 text-sm font-medium block sm:px-3 sm:py-2 sm:text-base capitalize
+                `}
+            >
+
+                {text}
+            </Link>
+        );
+    }
+
     return (
         <nav className="bg-gray-800">
-
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -37,25 +55,22 @@ const Navbar = () => {
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className={`
-                                            ${item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} 
-                                            rounded-md px-3 py-2 text-sm font-medium`
-                                        }
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
+                                {
+                                    navigationRoutes.map((singleRoute) => (
+                                        <NavigationLink
+                                            key={singleRoute}
+                                            href={`/${singleRoute}`}
+                                            text={singleRoute}
+                                        />
+                                    ))
+                                }
                             </div>
                         </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <a
-                            href="#"
+                            href={process.env?.NEXT_PUBLIC_GITHUB_LINK ?? ''}
+                            target='blank'
                             type="button"
                             className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                         >
@@ -68,20 +83,15 @@ const Navbar = () => {
 
             <div className={`sm:hidden transition-all duration-500 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
                 <div className="space-y-1 px-2 pb-3 pt-2">
-                    {navigation.map((item) => (
-                        <a
-                            key={item.name}
-                            type='button'
-                            href={item.href}
-                            className={`
-                                ${item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}
-                                block rounded-md px-3 py-2 text-base font-medium
-                            `}
-                            aria-current={item.current ? 'page' : undefined}
-                        >
-                            {item.name}
-                        </a>
-                    ))}
+                    {
+                        navigationRoutes.map((singleRoute) => (
+                            <NavigationLink
+                                key={singleRoute}
+                                href={`/${singleRoute}`}
+                                text={singleRoute}
+                            />
+                        ))
+                    }
                 </div>
             </div>
 
