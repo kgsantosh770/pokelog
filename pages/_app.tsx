33cb2c6b1fbaf8e27ef2efd5eb541bb0273e02d7
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-04-13 21:01:37 +0530

    folder structure changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { ApolloProvider } from '@apollo/client'
-import apolloClient from '@/utils/apolloClient'
+import apolloClient from '@/utils/api/services/apolloClient'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- utils/api/queries/getEvolution.ts
+++ utils/api/queries/getEvolution.ts
@@ -3,8 +3,6 @@ import { gql } from "@apollo/client";
 const GET_EVOLUTION = gql`
 query pokemon($id: String, $name: String){
     pokemon(id: $id, name: $name){
-      id
-      name
       evolutions{
         id
         number
