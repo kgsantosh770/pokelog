71170524885140283fb9bc35d67218ed28bfa537
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-04-15 10:57:41 +0530

    removed custom pulse animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,6 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      keyframes: {
-        pulse: {
-          '0% 100%': { backgroundColor: 'hsl(200, 20%, 50%)' },
-          '50%': { backgroundColor: 'hsl(200, 20%, 70%)' },
-        },
-      },
-      animation: {
-        skimmer: 'pulse 2s linear infinite',
-      }
     }
   },
   plugins: [],
