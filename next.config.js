b262c1715942a879507ebfd9d238f9f2047da152
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-04-17 02:00:21 +0530

    removed next/image domains config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 100,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.pokemondb.net',
-        port: '',
-        pathname: '/**',
-      },
-    ]
-  }
+  staticPageGenerationTimeout: 500,
 }
 
 module.exports = nextConfig
